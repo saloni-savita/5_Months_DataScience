ee17f86cead0350544a949dea46736183f007f05
Hey there, I'm Saloni! 👋 <salonisavita353@gmail.com>
2026-09-07 23:24:23 +0530

    feat: add basic rent calculator functionality

---
+++ RENT CALCULATOR.py
@@ -0,0 +1,26 @@
+## inputs we need  from the user 
+# total rent 
+# total food ordered for snacking 
+# electricity units spend 
+# charge per unit
+# person living in room/flat
+
+
+## output
+# total amount you've to pay is
+
+
+rent = int(input("Enter your hostel/flat rent = "))
+food = int(input("Enter the amount of food  ordered = "))
+electricity_spend = int(input("Enter the total of electricity spend = "))
+charge_per_unit = int(input("Enter the charge per unit = "))
+persons = int(input("Enter the number of persons living in room/flat = "))
+
+
+total_bill = electricity_spend * charge_per_unit
+
+output = (food + rent + total_bill) // persons
+  
+
+print("Each person will pay = ",output)
+
